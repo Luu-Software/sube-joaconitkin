3cb5afa320320d3fee294da2995190fbda3b954c
joaconitkin <joaconitkin@gmail.com>
2026-05-13 22:07:03 -0300

    hice el basico

--- main.js
+++ main.js
@@ -7,7 +7,42 @@ Distancia	  | Con SUBE registrada   | Con SUBE sin registrar
 6-12 km	    | $ 855.97		          | $ 1360.99
 12-27 km	  | $ 917.24		          | $ 1458.41
 */
+let registrada = document.getElementById("registrada");
+let distancia = document.getElementById("distancia");
+let boton = document.getElementById("boton");
+let tarifa = document.getElementById("tarifa");
 
 boton.addEventListener("click", () => {
-  // COMPLETAR
+let km = Number(distancia.value);
+let tipo = registrada.value;
+let precio = 0;
+if (tipo === "si") {
+  if (km >= 0 && km <= 3) {
+    precio = 715.24;
+  } else if (km > 3 && km <= 6) {
+    precio = 794.74;
+  } else if (km > 6 && km <= 12) {
+    precio = 855.97;
+  } else if (km > 12 && km <= 27) {
+    precio = 917.24;
+  } else {
+    tarifa.textContent = "Distancia no válida";
+    return;
+  }
+}
+else if (tipo === "no") {
+  if (km >= 0 && km <= 3) {
+    precio = 1137.23;
+  } else if (km > 3 && km <= 6) {
+    precio = 1263.64;
+  } else if (km > 6 && km <= 12) {
+    precio = 1360.99;
+  } else if (km > 12 && km <= 27) {
+    precio = 1458.41;
+  } else {
+    tarifa.textContent = "Distancia no válida";
+    return;
+  }
+}
+tarifa.textContent = `$ ${precio.toFixed(2)}`;
 });
